bc349ad599cb0cd7105654ade3cb402cc7423f36
decrent <mortalkombinat@gmail.com>
2022-11-17 20:00:20 +0700

    2. Сокобан. Тестовое задание.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
-        for (int diagonal = 0; diagonal < board.length; diagonal++) {
-            if (board[diagonal][diagonal] == 1) {
+        for (int askew = 0; askew < board.length; askew++) {
+            if (board[askew][askew] == 1) {
                 for (int i = 0; i < board.length; i++) {
-                    if (board[diagonal][i] != 0 || board[i][diagonal] != 0) {
+                    if (board[askew][i] != 0 || board[i][askew] != 0) {
                         rsl = true;
                     } else {
                         rsl = false;
